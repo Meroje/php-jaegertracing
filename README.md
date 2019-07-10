ae525c4ea4e282b1d6db5e9be948d60d9a549dfe
Jérôme Foray <moi@foray-jero.me>
2019-07-10 15:48:04 +0200

    subrequest sampling

---
+++ README.md
@@ -0,0 +1,6 @@
+# Opentracing PHP
+
+- PHP app: <http://localhost:8880/>
+- Jaeger UI: <http://localhost:16686/>
+- Jaeger admin: <http://localhost:14269/>
+- Prometheus: <http://localhost:9090/>
--- public/index.php
+++ public/index.php
@@ -2,16 +2,23 @@
 
 require __DIR__.'/../vendor/autoload.php';
 
+use GuzzleHttp\Client;
+use GuzzleHttp\Psr7\Request;
 use Jaeger\Config;
+use Jaeger\Tracer;
 use OpenTracing\GlobalTracer;
-use OpenTracing\Formats;
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
+use const Jaeger\SAMPLED_FLAG;
+use const Jaeger\SAMPLER_TYPE_CONST;
+use const OpenTracing\Formats\HTTP_HEADERS;
 
 if (!function_exists('getallheaders')) {
     function getallheaders()
     {
         $headers = [];
         foreach ($_SERVER as $name => $value) {
-            if (substr($name, 0, 5) == 'HTTP_') {
+            if (strpos($name, 'HTTP_') === 0) {
                 $headers[str_replace(' ', '-', strtolower(str_replace('_', ' ', substr($name, 5))))] = $value;
             }
         }
@@ -22,7 +29,7 @@ if (!function_exists('getallheaders')) {
 $config = new Config(
     [
         'sampler' => [
-            'type' => 'const',
+            'type' => SAMPLER_TYPE_CONST,
             'param' => true,
         ],
         'local_agent' => [
@@ -30,23 +37,46 @@ $config = new Config(
         ],
         'logging' => true,
     ],
-    'date-app'
+    'date-app',
+    new Logger('php', [new StreamHandler('php://stdout')])
 );
 $config->initializeTracer();
-/** @var \Jaeger\Tracer $tracer */
-$tracer = GlobalTracer::get();
+
+register_shutdown_function(static function() {
+    /* Flush the tracer to the backend */
+    $tracer = GlobalTracer::get();
+    $tracer->flush();
+});
+
 try {
+    /** @var Tracer $tracer */
+    $tracer = GlobalTracer::get();
+
     $spanContext = $tracer->extract(
-        Formats\HTTP_HEADERS,
+        HTTP_HEADERS,
         getallheaders()
     );
     if ($spanContext) {
-        $scope = $tracer->startActiveSpan('getDate', ['child_of' => $spanContext]);
-        $currentDateTime = date('Y-m-d H:i:s');
-        echo $currentDateTime;
-        $scope->close();
+        $requestScope = $tracer->startActiveSpan('request', ['child_of' => $spanContext]);
+
+        if (array_key_exists('getDate', $_GET)) {
+            $span = $tracer->startActiveSpan('getDate');
+            $currentDateTime = date('Y-m-d H:i:s');
+            echo $currentDateTime;
+            $span->close();
+        } else {
+            $span = $tracer->startActiveSpan('getDateRequest');
+            $client = new Client;
+            $headers = [];
+            $tracer->inject( $span->getSpan()->getContext(), HTTP_HEADERS, $headers);
+            $request = new Request('GET', 'http://nginx/?getDate', $headers);
+            $response = $client->send($request);
+            echo $response->getBody();
+            $span->close();
+        }
+
+        $requestScope->close();
     }
-    $tracer->flush();
 } catch (Exception $e) {
     var_dump($e);
 }
